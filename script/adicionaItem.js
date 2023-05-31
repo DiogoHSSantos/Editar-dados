02613aad29abede3649d4c7b1e1e231b1e869e0e
DiogoHSSantos <115512182+DiogoHSSantos@users.noreply.github.com>
2023-05-31 17:07:00 -0300

    criei uma forma melhor de organizar os codigos

---
+++ script/adicionaItem.js
@@ -0,0 +1,34 @@
+function adicionarItem(ulLista,input){
+    inItem = input.value;
+
+    const liLista = document.createElement('li');
+    liLista.classList.add('produto');
+    liLista.id = idAleatorio()
+
+    liLista.textContent = inItem;
+
+    let trash = document.createElement('i');
+    trash.classList.add('botaoExcluir');
+    trash.classList.add('bx');
+    trash.classList.add('bxs-trash-alt');
+
+    let checkBox = document.createElement('input');
+    checkBox.type = ('checkbox');
+    checkBox.classList.add('checkBox');
+
+    ulLista.appendChild(liLista);
+    liLista.appendChild(trash);
+    liLista.appendChild(checkBox);
+
+    let classeDeProdutos = document.querySelectorAll('.produto');
+    
+    item = classeDeProdutos;
+
+    return item;
+
+
+};
+
+function idAleatorio(){
+    return Math.floor(Math.random() * 1000);
+}
--- script/main.js
+++ script/main.js
@@ -1,52 +1,44 @@
 
 const nav = document.querySelector('.nav');
-
-let navButton = document.querySelector('#navButton');
-
+const navButton = document.querySelector('#navButton');
 const main = document.querySelector('main');
-
 
 navButton.addEventListener('click', () => {
     nav.classList.toggle('navActive');
 });
-
 main.addEventListener('click', () => {
     nav.classList.remove('navActive');
 });
 
 
-let adicionar = document.querySelector('.adicionar')
 
-adicionar.addEventListener('click', () => {
+const botaoAdicionar = document.querySelector('#adicionar');
+const input = document.querySelector('#itemLista');
+const ulLista = document.querySelector('#listaDeProdutos');
+const botaoLimparLista = document.querySelector('#limparLista');
 
-    let produtoIn = document.querySelector('#tdProduto')
+botaoAdicionar.addEventListener('click', ()=>{
+    adicionarItem(ulLista,input);
 
-    let produtosTr = document.createElement('tr');
-    produtosTr.classList.add('produtos')
+    let item = document.querySelectorAll('.produto')
 
-    let produtoTd = document.createElement('td');
-    let excluirTd = document.createElement('td');
-    let iconTrash = document.createElement('i');
-    iconTrash.classList.add('excluir')
-    iconTrash.classList.add('bx')
-    iconTrash.classList.add('bxs-trash-alt')
+    deletarItem(item)
 
-    excluirTd.appendChild(iconTrash);
+    input.value = '';
+    
+    
+});
+   
 
-    produtosTr.appendChild(produtoTd);
-    produtosTr.appendChild(excluirTd)
-
-    produtoTd.textContent = produtoIn.value;
-
-    let tabela = document.querySelector('.tabela');
-
-    tabela.appendChild(produtosTr);
-    let produtos = document.querySelectorAll('.produtos');
-
-        for(let i = 0 ; i < produtos.length ; i++){
-            excluir = produtos[i].querySelector('.excluir');
-            excluir.addEventListener('click', () => {
-                tabela.removeChild(produtos[i])
-    })  
+function deletarItem(item){
+    for(let i = 0 ; i < item.length ; i++){
+        let deletar =  item[i].querySelector('.botaoExcluir');
+        deletar.addEventListener('click',()=>{
+            return ulLista.removeChild(item[i]);
+        });
+    };
 }
-})
+
+botaoLimparLista.addEventListener('click', ()=>{
+    ulLista.innerHTML = ''
+  });
